ff328bad0199c9e1633b91b74fd26383e3ebedfe
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-18 16:14:08 -0400

    initial commit

---
+++ src/main/java/com/thechefsnotebook/thechefsnotebook/ThechefsnotebookApplication.java
@@ -0,0 +1,13 @@
+package com.thechefsnotebook.thechefsnotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThechefsnotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThechefsnotebookApplication.class, args);
+	}
+
+}
