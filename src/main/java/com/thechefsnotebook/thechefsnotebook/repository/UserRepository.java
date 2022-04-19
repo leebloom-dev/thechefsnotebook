c7e3e1b61d9364a61315f8a6434e29f761e0c21f
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-19 11:18:36 -0400

    edit CrudRepository param to Integer

--- src/main/java/com/thechefsnotebook/thechefsnotebook/repository/UserRepository.java
+++ src/main/java/com/thechefsnotebook/thechefsnotebook/repository/UserRepository.java
@@ -6,5 +6,5 @@ import com.thechefsnotebook.thechefsnotebook.model.User;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository.
 // CRUD refers Create, Read, Update, Delete.
 
-public interface UserRepository extends CrudRepository<User, Iterable> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 }
