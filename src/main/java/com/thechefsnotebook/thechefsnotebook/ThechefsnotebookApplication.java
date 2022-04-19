df7002c2208649e06b68957984cd602a5bfc078d
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-19 11:47:45 -0400

    added comments to TheChefsnotebookApplication.java

--- src/main/java/com/thechefsnotebook/thechefsnotebook/ThechefsnotebookApplication.java
+++ src/main/java/com/thechefsnotebook/thechefsnotebook/ThechefsnotebookApplication.java
@@ -11,3 +11,24 @@ public class ThechefsnotebookApplication {
 	}
 
 }
+
+/*
+* @SpringBootApplication
+* a convenience annotation that adds all of the following:
+*
+* @Configuration
+* Tags the class as a source of bean definitions for the application context.
+*
+* @EnableAutoConfiguration
+* Tells Spring Boot to start adding beans based on classpath settings,
+* other beans, and various property settings. For example, if spring-webmvc
+* is on the classpath, this annotation flags the application as a web
+* application and activates key behaviors, such as setting up a
+* DispatcherServlet.
+*
+* @ComponentScan
+* Tells Spring to look for other components, configurations, and service in
+* the com/thechefsnotebook package, letting it find the controllers.
+*
+* The main() method uses Spring Boot's SpringApplication.run() method
+* to launch an application.*/
