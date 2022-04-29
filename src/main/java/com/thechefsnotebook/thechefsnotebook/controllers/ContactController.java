e517f97ae2e40a23ba94e5c6b502e26b26885ca6
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-04-29 15:25:07 -0400

    add form

---
+++ src/main/java/com/thechefsnotebook/thechefsnotebook/controllers/ContactController.java
@@ -0,0 +1,29 @@
+package com.thechefsnotebook.thechefsnotebook.controllers;
+
+import com.thechefsnotebook.thechefsnotebook.models.Contact;
+import com.thechefsnotebook.thechefsnotebook.models.ContactBusiness;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class ContactController {
+
+    @GetMapping("/contact")
+    public String listContact(Model model) {
+
+        // Getting the list of contacts from Class ContactBusiness
+        ContactBusiness business = new ContactBusiness();
+        List<Contact> contactList = business.getContactList();
+
+        // adding the contact list to model's attribute "contacts"
+        model.addAttribute("contacts", contactList);
+
+        return "contact";
+
+    }
+
+}
