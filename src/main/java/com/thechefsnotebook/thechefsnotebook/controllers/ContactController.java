40e5159963fedf6053ca663f0aaf10365a08b65e
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-05-04 16:00:25 -0400

    refactored contact search feature

--- src/main/java/com/thechefsnotebook/thechefsnotebook/controllers/ContactController.java
+++ src/main/java/com/thechefsnotebook/thechefsnotebook/controllers/ContactController.java
@@ -20,9 +20,54 @@ public class ContactController {
     // Lives at /contact
     @GetMapping
     public String displayAllContacts(Model model) {
+        // displaying conditional html
+        boolean isSearching = false;
+
+        model.addAttribute("isSearching", isSearching);
         model.addAttribute("title", "Contact");
         model.addAttribute("contacts", contacts);
         return "/contacts/contact"; // HTML path
+    }
+
+    // Lives at /contact
+    @PostMapping
+    public String createSearchContact(@RequestParam String contactFirstName,
+                                      @RequestParam String contactLastName,
+                                      Model model) {
+        // for displaying html
+        boolean isSearching = true;
+        model.addAttribute("isSearching", isSearching);
+
+        // Empty Array List to hold searched contacts
+        List<Contact> contactsSearched = new ArrayList<>();
+
+        // TODO: Include a unique id for each contact.
+        // TODO: Delete contact(s) feature
+
+        for (Contact contact : contacts) {
+
+            // Condition: first name match AND last name empty
+            if (contact.getFirstName().equalsIgnoreCase(contactFirstName) && contactLastName.isEmpty()) {
+                contactsSearched.add(contact);
+                model.addAttribute("contacts", contactsSearched);
+            }
+
+            // Condition: first name empty AND last name match
+            if (contactFirstName.isEmpty() && contact.getLastName().equalsIgnoreCase(contactLastName)) {
+                contactsSearched.add(contact);
+                model.addAttribute("contacts", contactsSearched);
+            }
+
+            // Condition: first name AND last name match
+            if (contact.getFirstName().equalsIgnoreCase(contactFirstName) && contact.getLastName().equalsIgnoreCase(contactLastName)) {
+                contactsSearched.add(contact);
+                model.addAttribute("contacts", contactsSearched);
+            }
+
+        }
+
+        return "/contacts/contact";
+
     }
 
     // Lives at /contact/form
@@ -48,50 +93,6 @@ public class ContactController {
         );
 
         return "redirect:"; // redirect to this class's URL path
-
-    }
-
-    // Lives at /contact/search
-    @GetMapping("search")
-    public String renderSearchContact() {
-        return "/contacts/contactSearch";
-    }
-
-    // Lives at /contact/search
-    @PostMapping("search")
-    public String createSearchContact(@RequestParam String contactFirstName,
-                                      @RequestParam String contactLastName,
-                                      Model model) {
-
-        // Empty Array List to hold searched contacts
-        List<Contact> searchContacts = new ArrayList<>();
-
-        // TODO: Include a unique id for each contact.
-        // TODO: Delete contact(s) feature
-
-        for (Contact contact : contacts) {
-
-            // Condition: first name match AND last name empty
-            if (contact.getFirstName().equalsIgnoreCase(contactFirstName) && contactLastName.isEmpty()) {
-                searchContacts.add(contact);
-                model.addAttribute("contacts", searchContacts);
-            }
-
-            // Condition: first name empty AND last name match
-            if (contactFirstName.isEmpty() && contact.getLastName().equalsIgnoreCase(contactLastName)) {
-                searchContacts.add(contact);
-                model.addAttribute("contacts", searchContacts);
-            }
-
-            // Condition: first name AND last name match
-            if (contact.getFirstName().equalsIgnoreCase(contactFirstName) && contact.getLastName().equalsIgnoreCase(contactLastName)) {
-                searchContacts.add(contact);
-                model.addAttribute("contacts", searchContacts);
-            }
-
-        }
-
-        return "/contacts/contactSearch";
 
     }
 
