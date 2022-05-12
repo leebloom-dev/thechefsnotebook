10b8d6dddf8f686182e6b2432cd4152e42606f4a
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-05-12 13:21:17 -0400

    model validation

--- src/main/java/com/thechefsnotebook/thechefsnotebook/controllers/RecipeController.java
+++ src/main/java/com/thechefsnotebook/thechefsnotebook/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.thechefsnotebook.thechefsnotebook.controllers;
 
+import javax.validation.Valid;
+
 import com.thechefsnotebook.thechefsnotebook.data.RecipeData;
 import com.thechefsnotebook.thechefsnotebook.model.Recipe;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,13 +34,22 @@ public class RecipeController {
 
     // responds to POST requests at "/recipe/form"
     @PostMapping("form")
-    public String processRecipeForm(@ModelAttribute Recipe newRecipe) {
+    public String processRecipeForm(@ModelAttribute @Valid Recipe newRecipe,
+                                    Errors errors,
+                                    Model model) {
+
+        // Handles validation errors by preventing validation exception.
+        if (errors.hasErrors()) {
+            model.addAttribute("Title", "Recipe Form");
+            model.addAttribute("errorMsg", "Bad data!");
+            return "recipes/form";
+        }
 
         // Using @ModelAttribute instead of @RequestParam to pass user input into the Recipe object.
         // Passed input goes through the object's constructor. Field names must match HTML name
         // attribute.
         RecipeData.add(newRecipe);
-        return "redirect:/recipe"; // redirect to "/recipe"
+        return "redirect:"; // redirect to "/recipe"
 
     }
 
--- src/main/java/com/thechefsnotebook/thechefsnotebook/model/Recipe.java
+++ src/main/java/com/thechefsnotebook/thechefsnotebook/model/Recipe.java
@@ -1,8 +1,12 @@
 package com.thechefsnotebook.thechefsnotebook.model;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Recipe {
@@ -11,7 +15,12 @@ public class Recipe {
     @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name is required.") // message displayed in Whitelabel error page
+    @Size(min = 3, max = 50, message = "Name must be within 3 to 50 characters.")
     private String recipeName;
+
+    @NotBlank(message = "Nmae is required.")
+    @Size(min = 3, max = 50, message = "name must be within 3 to 50 characters.")
     private String cuisineType;
 
     public Recipe(String recipeName, String cuisineType) {
@@ -37,6 +46,24 @@ public class Recipe {
 
     public void setCuisineType(String cuisineType) {
         this.cuisineType = cuisineType;
+    }
+
+    @Override
+    public String toString() {
+        return recipeName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Recipe recipe = (Recipe) obj;
+        return id == recipe.id;
     }
 
 }
