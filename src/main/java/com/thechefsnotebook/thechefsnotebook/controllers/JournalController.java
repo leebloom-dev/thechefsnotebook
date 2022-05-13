6181aae951d22503d00250c35f72a4d200c256ed
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-05-13 15:23:13 -0400

    journal - included links for software pre-requisites for creating this web application

--- src/main/java/com/thechefsnotebook/thechefsnotebook/controllers/JournalController.java
+++ src/main/java/com/thechefsnotebook/thechefsnotebook/controllers/JournalController.java
@@ -6,12 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("journal")
-public class BlogController {
+public class JournalController {
 
     // responds to GET requests at "/journal"
     @GetMapping
     public String renderIndex() {
         return "journal/index";
+    }
+
+    // responsds to GET requests at "/journal/getting-started"
+    @GetMapping("/getting-started")
+    public String renderGettingStarted() {
+        return "journal/getting-started";
     }
 
 }
