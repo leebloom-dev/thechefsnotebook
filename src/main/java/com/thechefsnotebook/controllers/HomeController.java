ba01356d6fecda0c877ef4780874cdf117724ae1
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-05-26 14:38:18 -0400

    basic html and controller for home page

---
+++ src/main/java/com/thechefsnotebook/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.thechefsnotebook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String displayHomePage() {
+        return "index";
+    }
+    
+}
