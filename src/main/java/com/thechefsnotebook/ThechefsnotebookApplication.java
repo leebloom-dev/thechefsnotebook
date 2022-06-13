8b6764549140cf95a11a35af5f01459a65799c67
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-06-13 18:30:31 -0400

    gitignore application properties

--- src/main/java/com/thechefsnotebook/ThechefsnotebookApplication.java
+++ src/main/java/com/thechefsnotebook/ThechefsnotebookApplication.java
@@ -3,6 +3,7 @@ package com.thechefsnotebook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ThechefsnotebookApplication {
 
