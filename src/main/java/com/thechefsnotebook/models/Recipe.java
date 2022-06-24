135c6d62ca70688af82ce6707d08685ad4c67bec
Andrew Lee Bloom <leebloom@Lees-MBP-M1.lan>
2022-06-24 14:59:19 -0400

    included an abstract class for models.

--- src/main/java/com/thechefsnotebook/models/Recipe.java
+++ src/main/java/com/thechefsnotebook/models/Recipe.java
@@ -1,21 +1,13 @@
 package com.thechefsnotebook.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
-public class Recipe {
+public class Recipe extends AbstractEntity {
 
-    @Id
-    @GeneratedValue
-    private int id;
-
-    @NotBlank(message = "Must give a recipe name!")
-    // Size in characters
-    
+    @NotBlank(message = "Must give a recipe name!")  // Size in characters
     @Size(min = 3, max = 50, message = "Must be within 3 to 50 characters!")
     private String name;
 
@@ -28,10 +20,6 @@ public class Recipe {
     }
 
     public Recipe() {
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getName() {
@@ -50,21 +38,9 @@ public class Recipe {
         this.cuisine = cuisine;
     }
     
-    // @Override
-    // public String toString() {
-    //     return name;
-    // }
+    @Override
+    public String toString() {
+        return name;
+    }
 
-    // @Override
-    // public boolean equals(Object o) {
-    //     if (this == o) return true;
-    //     if (o == null || getClass() != o.getClass()) return false;
-    //     Recipe recipe = (Recipe) o;
-    //     return id == recipe.id;
-    // }
-
-    // @Override
-    // public int hashCode() {
-    //     return Objects.hash(id);
-    // }
 }
