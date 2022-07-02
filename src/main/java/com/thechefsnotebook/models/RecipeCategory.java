7c04ff575f7e8ac3016a0f687ea7dbebff5b4da1
Lee <leebloomdev@gmail.com>
2022-07-02 17:30:17 -0400

    User can filter recipes by category by clicking the links on the category page.

--- src/main/java/com/thechefsnotebook/models/RecipeCategory.java
+++ src/main/java/com/thechefsnotebook/models/RecipeCategory.java
@@ -1,6 +1,10 @@
 package com.thechefsnotebook.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -10,6 +14,9 @@ public class RecipeCategory extends AbstractEntity {
     @NotBlank(message = "The recipe category can not be blank!")
     @Size(min = 3, max = 50, message = "Must be within 3 to 50 characters!")
     private String name;
+
+    @OneToMany(mappedBy = "recipeCategory")
+    private final List<Recipe> recipes = new ArrayList<>();
 
     public RecipeCategory(String name) {
         this.name = name;
@@ -24,6 +31,10 @@ public class RecipeCategory extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
     }
 
  }
